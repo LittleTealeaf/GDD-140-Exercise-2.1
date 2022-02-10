b09641230c3e5cbcfcc4aac7f16d1d26d69a7bfe
Thomas Kwashnak <shintaomonk@gmail.com>
2022-02-10 15:46:53 -0500

    Colors!

--- sketch.js
+++ sketch.js
@@ -2,9 +2,15 @@
 
 var x = 0;
 var y = 0;
-var dx = 1;
-var dy = -1;
+var dx = 0;
+var dy = 0;
 var radius = 25;
+var fillR = 0;
+var fillG = 0;
+var fillB = 0;
+var fillRGoal = 0;
+var fillGGoal = 0;
+var fillBGoal = 0;
 
 function setup() {
   createCanvas(400, 400);
@@ -14,13 +20,42 @@ function setup() {
   x = random(radius, 400 - radius);
   y = random(radius, 400 - radius);
   //Set ellipse random velocity
-  dx = random(0, 3);
-  dy = random(0, 3);
+  dx = random(-5, 5);
+  dy = random(-5, 5);
+  background(220);
+  noStroke();
 }
 
 function draw() {
-  background(220);
+  updateFill();
   updateEllipse();
+}
+
+function updateFill() {
+  //Update red component. If it has reached the red component, then make a new goal
+  if(fillR > fillRGoal) {
+    fillR--;
+  } else if(fillR < fillRGoal) {
+    fillR++;
+  } else {
+    fillRGoal = int(random(255));
+  }
+  if(fillG > fillGGoal) {
+    fillG--;
+  } else if(fillG < fillGGoal) {
+    fillG++;
+  } else {
+    fillGGoal = int(random(255));
+  }
+  if(fillB > fillBGoal) {
+    fillB--;
+  } else if(fillB < fillBGoal) {
+    fillB++;
+  } else {
+    fillBGoal = int(random(255));
+  }
+
+  fill(fillR,fillG,fillB);
 }
 
 function updateEllipse() {
